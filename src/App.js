60e67f610604cbfa6aeef7b3c8fb8dde02d014f0
Dang Duc Chinh <chinh22082001@gmail.com>
2022-03-13 21:50:16 +0700

    LAB03_1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
+import React, {Component} from 'react';
 import logo from './logo.svg';
-import './App.css';
 import {Navbar , NavbarBrand} from 'reactstrap';
+import Menu from './components/MenuComponent' ; 
+import './App.css';
+
 function App() {
   return (
     <div className="App">
       <Navbar dark color="primary">
-        <div class="container">
-        <NavbarBrand className='class-1'>Thư mục 1</NavbarBrand>
-        <NavbarBrand className='class-1'>Thư mục 2</NavbarBrand>
+        <div className="container">
+          <NavbarBrand className='class-1'>Thư mục 1</NavbarBrand>
+          <NavbarBrand className='class-1'>Thư mục 2</NavbarBrand>
         </div>
       </Navbar>
+      <Menu />
     </div>
   );
 }
