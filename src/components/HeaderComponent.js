f231234a024a29e0593f66c9c311726fd9ccc0bc
Dang Duc Chinh <chinh22082001@gmail.com>
2022-03-26 09:54:36 +0700

    LAB06_2

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,69 +1,106 @@
 import React, { Component } from "react";
-import { Navbar, NavbarBrand, Nav, NavItem, NavbarToggler, Collapse, Jumbotron } from 'reactstrap';
+import {
+    Navbar, NavbarBrand, Nav, NavItem, NavbarToggler, Collapse, Jumbotron,
+    Modal, ModalHeader, ModalBody, FormGroup, Input, Label, Button, Form
+} from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 
 class Header extends Component {
-    constructor(props){
-        super(props) ; 
+    constructor(props) {
+        super(props);
         this.state = {
-            isNavOpen: false
+            isNavOpen: false,
+            isModalOpen: false
         }
-        this.toggleNav = this.toggleNav.bind(this) ; 
+
+        this.toggleNav = this.toggleNav.bind(this);
+        // this.toggleModal = this.toggleModal.bind(this);
+        // this.handleLogin = this.handleLogin.bind(this);
+        // // ràng buộc 
     }
 
-    toggleNav(){
+    toggleNav() {
         this.setState({
             isNavOpen: !this.state.isNavOpen
         });
     }
+    // toggleModal() {
+    //     this.setState({
+    //         isModalOpen: !this.state.isModalOpen
+    //     });
+    // }
+    // handleLogin(event) {
+    //     this.toggleModal();
+    //     alert("Username: " + this.username.value + " Password: " + this.password.value
+    //         + " Remember: " + this.remember.checked);
+    //     event.preventDefault();
+    // }
 
     render() {
         // <>  == React.Fragment ?
         return (
             <>
-                <Navbar dark expand="md">
-                    <div className='container'>
-                        <NavbarToggler onClick={this.toggleNav} />
-                        <NavbarBrand className="me-auto" href="/">
-                            <img src="assets/images/logo.png" alt="Logo-Mukbag" height="30" width="41" />
-                        </NavbarBrand>
-                        <Collapse navbar isOpen={this.state.isNavOpen}>
-                            <Nav navbar>  
-                                <NavItem>
-                                    <NavLink className="nav-link" to="/home">
-                                        <span className="fa fa-home fa-lg"></span>Home
-                                    </NavLink>
-                                    <NavLink className="nav-link" to="/aboutus">
-                                        <span className="fa fa-info fa-lg"></span>About Us
-                                    </NavLink>
-                                    <NavLink className="nav-link" to="/menu">
-                                        <span className="fa fa-list fa-lg"></span>Menu
-                                    </NavLink>
-                                    <NavLink className="nav-link" to="/contactus">
-                                        <span className="fa fa-address-card fa-lg"></span>Contact Us
-                                    </NavLink>
-                                </NavItem>
-                            </Nav>
-                        </Collapse>
-                    </div>
+                <Navbar dark expand="md" style={{ paddingLeft: '40px' }}>
+                    <NavbarToggler onClick={this.toggleNav} />
+                    <NavbarBrand className="me-auto" href="/">
+                        <img src="assets/images/logo.png" alt="Logo-Mukbag" height="30" width="41" />
+                    </NavbarBrand>
+                    <Collapse navbar isOpen={this.state.isNavOpen}>
+                        <Nav navbar>
+                            <NavItem>
+                                <NavLink className="nav-link" to="/home">
+                                    <span className="fa fa-home fa-lg"></span>Home
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink className="nav-link" to="/aboutus">
+                                    <span className="fa fa-info fa-lg"></span>About Us
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink className="nav-link" to="/menu">
+                                    <span className="fa fa-list fa-lg"></span>Menu
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink className="nav-link" to="/contactus">
+                                    <span className="fa fa-address-card fa-lg"></span>Contact Us
+                                </NavLink>
+                            </NavItem>
+                        </Nav>
+                        {/* căn lền trái càng nhiều càng tốt */}
+                    </Collapse>
+                    <Nav className="ml-auto" navbar>
+                        <NavItem>
+                            <Button outline onClick={this.toggleModal}><span className="fa fa-sign-in fa-lg"></span> Login</Button>
+                        </NavItem>
+                    </Nav>
                 </Navbar>
-                {/* <Jumbotron>
-                    <div className="container">
-                        <div className="row row-header">
-                            <div className="col-12 col-sm-6">
-                            <h1>Ristorante Con Fusion</h1>
-                                <p>We take inspiration from the World's best suiscine and serious. But i can help you maeke restaurant in the Mukabbang at outside</p>
-                            </div>
-                        </div>
-                    </div>
-                </Jumbotron> */}
-                <div className="container-fluid bg-light text-dark p-5">
-                    <div className="container bg-light p-1">
-                        <h1 className="display-4 fw-bold">Welcome to React Application v1.0</h1>
-                        <p>Go to My Website</p>
-                        <a href="#" className="btn btn-primary">link</a>
-                    </div>
-                </div>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+                    <ModalBody>
+                    <Form onSubmit={this.handleLogin}>
+                            <FormGroup>
+                                <Label htmlFor="username">Username</Label>
+                                <Input type="text" id="username" name="username"
+                                    innerRef={(input) => this.username = input} />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor="password">Password</Label>
+                                <Input type="password" id="password" name="password"
+                                    innerRef={(input) => this.password = input}  />
+                            </FormGroup>
+                            <FormGroup check>
+                                <Label check>
+                                    <Input type="checkbox" name="remember"
+                                    innerRef={(input) => this.remember = input}  />
+                                    Remember me
+                                </Label>
+                            </FormGroup>
+                            <Button type="submit" value="submit" color="primary">Login</Button>
+                        </Form>
+                    </ModalBody>
+                </Modal>
             </>
         );
     }
